2f0ed89f1c3235ea43b8c90b7173ed1d75cbd0ef
ndangles <nicholasdangles@gmail.com>
2019-11-01 20:11:05 -0500

    fixed alert error for web server, ran prettier

--- index.js
+++ index.js
@@ -1,64 +1,75 @@
 const path = require("path");
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const discord_client = new Discord.Client();
 
-let user_config = {}
+let user_config = {};
 let discordLoggedIn = false;
 
 exports.configure = function(file) {
-		try {
-			user_config = require(path.resolve(`${__dirname}/../../${file}`));
-			template_config = require(__dirname + "/simplert.cfg.json")
+  try {
+    user_config = require(path.resolve(`${__dirname}/../../${file}`));
+    template_config = require(__dirname + "/simplert.cfg.json");
 
-			user_keys = getAllKeys(user_config)
-			template_keys = getAllKeys(template_config)
+    user_keys = getAllKeys(user_config);
+    template_keys = getAllKeys(template_config);
 
-			if(user_keys.join() !== template_keys.join()) throw "Your simplert config file is not up to date, use the generate command to get the most up to date version"
-
-		} catch(e) {
-			if(e.code === "MODULE_NOT_FOUND") {
-				throw new Error("Could not file specified. Make sure file path is correct.")
-			} else {
-				throw new Error(e) 
-			}
-		}
-}
+    if (user_keys.join() !== template_keys.join())
+      throw "Your simplert config file is not up to date, use the generate command to get the most up to date version";
+  } catch (e) {
+    if (e.code === "MODULE_NOT_FOUND") {
+      throw new Error(
+        "Could not file specified. Make sure file path is correct."
+      );
+    } else {
+      throw new Error(e);
+    }
+  }
+};
 
 exports.discord = function(message, send_to = user_config.discord.send_to) {
-	return new Promise((resolve, reject) => {
-			let error;
+  return new Promise((resolve, reject) => {
+    let error;
 
-			if(!user_config.discord.enabled) error = "Discord alert is not enabled. You need to enable it in your simplert config file"
+    if (!user_config.discord.enabled)
+      error =
+        "Discord alert is not enabled. You need to enable it in your simplert config file";
+    else if (!message)
+      error =
+        ".discord() requires at least 1 argument. Pass it a message to send to Discord";
+    else if (!send_to)
+      error =
+        "You need to specify a Discord channel either in your simplert config file or passed into .discord()";
 
-			else if(!message) error = ".discord() requires at least 1 argument. Pass it a message to send to Discord"
+    if (error) reject(new Error(error));
 
-			else if(!send_to) error = "You need to specify a Discord channel either in your simplert config file or passed into .discord()"
+    if (!discordLoggedIn) {
+      discordLoggedIn = true;
+      discord_client.login(user_config.discord.token).catch(error => {
+        reject(error);
+      });
 
-			if(error) reject(new Error(error))
-
-			if(!discordLoggedIn) {
-				discordLoggedIn = true
-				discord_client.login(user_config.discord.token).catch(error => {
-					reject(error)
-				});
-			}
-
-			discord_client.on('ready', () => {
-				const channel = discord_client.channels.find(channel => channel.name === send_to);
-				discord_client.channels.get(channel.id).send(message);
-				resolve();
-			});
-		})
-}
-
+      discord_client.on("ready", () => {
+        const channel = discord_client.channels.find(
+          channel => channel.name === send_to
+        );
+        discord_client.channels.get(channel.id).send(message);
+        resolve();
+      });
+    } else {
+      const channel = discord_client.channels.find(
+        channel => channel.name === send_to
+      );
+      discord_client.channels.get(channel.id).send(message);
+      resolve();
+    }
+  });
+};
 
 function getAllKeys(data) {
+  const parent_keys = Object.keys(data);
+  let child_keys = [];
 
-			const parent_keys = Object.keys(data);
-			let child_keys = []
-
-			for(pkey of parent_keys) child_keys = child_keys.concat(Object.keys(data[pkey])) 
-			return parent_keys.concat(child_keys).sort()
-			
+  for (pkey of parent_keys)
+    child_keys = child_keys.concat(Object.keys(data[pkey]));
+  return parent_keys.concat(child_keys).sort();
 }
-
