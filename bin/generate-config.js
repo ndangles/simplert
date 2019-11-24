457bfc0a0ef5565acb1775cbcd55ad6310473b40
ndangles <nicholasdangles@gmail.com>
2019-11-23 21:28:22 -0500

    added script to generate token for a gmail account

--- bin/generate-config.js
+++ bin/generate-config.js
@@ -1,23 +1,30 @@
 #!/usr/bin/env node
 
 const path = require("path");
-const DEFAULT_CONFIG_NAME = "simplert.cfg.json"
+const DEFAULT_CONFIG_NAME = "simplert.cfg.json";
 let config_name = DEFAULT_CONFIG_NAME;
 
-const fs = require('fs');
-const readline = require('readline').createInterface({
+const fs = require("fs");
+const readline = require("readline").createInterface({
   input: process.stdin,
   output: process.stdout
-})
+});
 
-readline.question(`Name your config file (${DEFAULT_CONFIG_NAME}) `, (filename) => {
-	if(filename) config_name = filename;
+readline.question(
+  `Name your config file (${DEFAULT_CONFIG_NAME}) `,
+  filename => {
+    if (filename) config_name = filename;
 
-	fs.copyFile(path.resolve(__dirname + "/..") + `/${DEFAULT_CONFIG_NAME}`, config_name, fs.constants.COPYFILE_EXCL, (err) => {
-		if (err) throw err;
-		console.log(`${config_name} was successfully generated`);
-	});
+    fs.copyFile(
+      path.resolve(__dirname + "/..") + `/${DEFAULT_CONFIG_NAME}`,
+      config_name,
+      fs.constants.COPYFILE_EXCL,
+      err => {
+        if (err) throw err;
+        console.log(`${config_name} was successfully generated`);
+      }
+    );
 
-  readline.close()
-})
-
+    readline.close();
+  }
+);
---
+++ bin/generate-gmail-creds.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+const path = require("path");
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const { google } = require("googleapis");
+
+const SCOPES = ["https://www.googleapis.com/auth/gmail.send"];
+
+readline.question(
+  "Path to your simplert configuration file: ",
+  simplertCfgFile => {
+    const simplert_config = require(path.resolve(
+      __dirname + "/../" + simplertCfgFile
+    ));
+
+    readline.question(
+      "Path to your gmail-credetials.json file: ",
+      gmailCredsFile => {
+        const {
+          installed: { client_secret, client_id, redirect_uris }
+        } = require(path.resolve(__dirname + "/../" + gmailCredsFile));
+        const oAuth2Client = new google.auth.OAuth2(
+          client_id,
+          client_secret,
+          redirect_uris[0]
+        );
+
+        const authUrl = oAuth2Client.generateAuthUrl({
+          access_type: "offline",
+          scope: SCOPES
+        });
+
+        console.log("Authorize this app by visiting this url:", authUrl);
+        readline.question("Enter the code from that page here: ", code => {
+          readline.close();
+          oAuth2Client.getToken(code, (err, token) => {
+            if (err) {
+              console.error("Error retrieving access token", err);
+              process.exit(1);
+            }
+
+            simplert_config.email.gmail.token = token;
+            fs.writeFile(
+              simplertCfgFile,
+              JSON.stringify(simplert_config, null, 2),
+              function(err) {
+                if (err) return console.error(err);
+                console.log("Simplert config file updated with Gmail token");
+              }
+            );
+          });
+        });
+      }
+    );
+  }
+);
